7f1a07bda81df1884eec27d3f8e66eb2e4e87100
kutty selva <kuttyselva57@gmail.com>
2019-06-19 23:56:45 +0530

    final

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Launch from './components/Launch';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 
 const client=new ApolloClient({
-  uri:'http://localhost:5000/graphql'
+  uri:'/graphql'
 })
 class App extends Component {
   render(){
---
+++ public/precache-manifest.4c58b1662dbaab0a6c97d4707582e749.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe1bc140c2f71ff94629f42c53d10c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15f83dc98819a320cf22",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4b9088386f31415392d7",
+    "url": "/static/js/2.ba88e476.chunk.js"
+  },
+  {
+    "revision": "15f83dc98819a320cf22",
+    "url": "/static/js/main.27b41cd7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
